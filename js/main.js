7b9f689fcbb1e124829db00b839fcb2023e6b2e7
chamara-g <kggcbgunarathne@gmail.com>
2019-12-23 21:30:46 +0530

    page top button change

--- js/main.js
+++ js/main.js
@@ -65,4 +65,19 @@
 	    $('#target').show(0);
 	    $('.home').hide(0);
 	});*/
-		
+
+/*page top*/		
+var btn = $('#buttonPageTop');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
